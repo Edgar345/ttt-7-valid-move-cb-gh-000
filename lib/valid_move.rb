d936363d19a25b0b1cbb536df88c384ec1cbd59e
Edgar V <evenegas9@ucmerced.edu>
2019-08-30 23:55:37 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -9,3 +9,10 @@ def position_taken? board, index
     true
   end
 end
+
+def valid_move? board, index
+  if (index >= 0 && index <= 8) && !position_taken(board, index)
+    true
+  else 
+    false
+end
